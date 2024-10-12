aa35a50a5912799c0bb52090d1de83a23d5e7463
Somya Bagora <19ucc052@lnmiit.ac.in>
2024-10-12 14:50:44 +0530

    Initial commit

---
+++ engine/src/index.ts
@@ -0,0 +1,24 @@
+import { createClient, } from "redis";
+import { Engine } from "./trade/Engine";
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+async function main() {
+    const engine = new Engine(); 
+    const redisClient = createClient();
+    await redisClient.connect();
+    console.log("connected to redis");
+
+    while (true) {
+        const response = await redisClient.rPop("messages" as string)
+        if (!response) {
+
+        }  else {
+            engine.process(JSON.parse(response));
+        }        
+    }
+
+}
+
+main();
